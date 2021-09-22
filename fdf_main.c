c4d1f9ccd354ec6cae9be3b155e316ea99d51c19
Abdelaziz Belhaj <abel-haj@s2r5p5.1337.ma>
2021-09-22 19:45:28 +0100

    started gathering data

--- fdf_main.c
+++ fdf_main.c
@@ -1,23 +1,40 @@
 #include "fdf.h"
 #include <stdio.h>
 
-int		main(int argc, char **argv)
+typedef struct s_fdf_data
 {
+	int	width;
+	int	height;
+	int	**map;
+} t_fdf_data;
+
+int	get_map_width(char *line)
+{
+	
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
 	int		fd;
 	char	*line;
+	char	*tmp;
+	t_fdf_data t_data;
 
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd != -1)
+		if (fd != -1 || ft_strcmp(ft_strchr(argv[1], '.'), ".fdf"))
 		{
+			free(line);
+
+			t_data.width = get_map_width(line);
+
 			while (get_next_line(fd, &line) > 0)
 			{
-				printf("%s\n", line);
 				free(line);
+				t_data.height++;
 			}
-			printf("%s", line);
-			free(line);
 		}
 		else
 		{
